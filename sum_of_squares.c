ca9f4804213b6c242b8676249884de7a11de9028
Saha047 <sahasoumyajit906@gmail.com>
2025-02-13 16:46:28 +0530

    Add files via upload

---
+++ sum_of_squares.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10; 
+    int sum_of_squares = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    printf("The sum of the squares of the first %d natural numbers is: %d\n", n, sum_of_squares);
+    return 0;
+}
